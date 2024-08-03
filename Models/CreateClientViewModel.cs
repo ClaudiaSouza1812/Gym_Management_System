c6861cd3a050446fe76e982465155bb74edf22cc
Claudia Simone de Souza. <claudia.simmone@gmail.com>
2024-08-03 16:35:33 +0100

    240803

---
+++ Models/CreateClientViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace P02_2_ASP.NET_Core_MVC_M01_ClaudiaSouza.Models
+{
+    public class CreateClientViewModel
+    {
+        public Client Client { get; set; } = new Client();
+        public List<Membership> Membership { get; set; } = new List<Membership>();
+        public List<Contract> Contract { get; set; } = new List<Contract>();
+        public List<Payment> Payment { get; set; } = new List<Payment>();
+        public List<Modality> Modality { get; set; } = new List<Modality>();
+
+    }
+}
